746f1d2bcbbcbae2b9547c79cb2565766d91b721
Felipe Carvalho <felipe.carvalho@gmail.com>
2016-10-05 21:14:06 +0200

    new test

--- gossiping-bus-drivers/src/test/scala/com/github/felipecao/katas/gossip/DriversFactoryTest.scala
+++ gossiping-bus-drivers/src/test/scala/com/github/felipecao/katas/gossip/DriversFactoryTest.scala
@@ -24,4 +24,18 @@ class DriversFactoryTest extends FlatSpec with Matchers {
       )
     )
   }
+
+  "DriversFactory" should "build a collection of two drivers with two routes each" in {
+    def routesCollection = Seq(Seq(1, 2), Seq(2, 1))
+    def drivers = DriverFactory.buildFromRoutesCollection(routesCollection)
+
+    drivers.count should be (2)
+    drivers.allDrivers should be (
+      Seq(
+        Driver(Route(Seq(1, 2))),
+        Driver(Route(Seq(2, 1)))
+      )
+    )
+  }
+
 }
