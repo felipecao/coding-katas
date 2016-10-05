d8af3448df7685aead8fefe67765f873df28ae4b
Felipe Carvalho <felipe.carvalho@gmail.com>
2016-10-05 21:21:41 +0200

    refactor

--- gossiping-bus-drivers/src/main/scala/com/github/felipecao/katas/gossip/DriverFactory.scala
+++ gossiping-bus-drivers/src/main/scala/com/github/felipecao/katas/gossip/DriverFactory.scala
@@ -1,17 +1,10 @@
 package com.github.felipecao.katas.gossip
 
-import scala.collection.mutable.ListBuffer
-
 object DriverFactory {
   def buildFromRoutesCollection(routesOfEachDriver: Seq[Seq[Int]]): Drivers = {
-
-    var drivers = new ListBuffer[Driver]()
-    
-    routesOfEachDriver.foreach( routesOfOneDriver =>
-      drivers += new Driver(Route(routesOfOneDriver))
+    new Drivers(
+      routesOfEachDriver
+        .map(routesOfOneDriver => new Driver(Route(routesOfOneDriver)))
     )
-
-    return new Drivers(drivers)
   }
-  
 }
