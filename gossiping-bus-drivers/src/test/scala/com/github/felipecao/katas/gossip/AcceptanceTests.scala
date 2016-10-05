56a615e5b05132130bcc1d92b5a236b81b31a482
Felipe Carvalho <felipe.carvalho@gmail.com>
2016-10-05 19:36:09 +0200

    new test

--- gossiping-bus-drivers/src/test/scala/com/github/felipecao/katas/gossip/AcceptanceTests.scala
+++ gossiping-bus-drivers/src/test/scala/com/github/felipecao/katas/gossip/AcceptanceTests.scala
@@ -6,15 +6,23 @@ import scala.util.Random
 
 class AcceptanceTests extends FlatSpec with Matchers {
 
-  def getOutputForRoutesCollection(routesCollection: Seq[Int]): String = {
+  def getOutputForRoutesCollection(routesCollection: Seq[Seq[Int]]): String = {
     "never"
   }
 
   "A collection of routes with just one driver" should "output 'never' regardless of the number of stops" in {
-    def routesCollection = Seq.fill(Random.nextInt(100))(Random.nextInt(100))
+    def routesCollection = Seq(Seq.fill(Random.nextInt(100))(Random.nextInt(100)))
     def output = getOutputForRoutesCollection(routesCollection)
 
     output should be ("never")
+  }
+
+  "A collection of routes with two drivers with one stop and both have the same stop" should "output '1'" in {
+    def busStop = 1
+    def routesCollection = Seq(Seq(busStop), Seq(busStop))
+    def output = getOutputForRoutesCollection(routesCollection)
+
+    output should be ("1")
   }
 
 }
