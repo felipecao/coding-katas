cc2d12fbb201f2207637b6694850cd398637809a
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-02-01 12:46:18 -0200

    Refactored Game to enable calculation of scores for both players

--- tennis/src/main/groovy/au/com/dius/tennis/Game.groovy
+++ tennis/src/main/groovy/au/com/dius/tennis/Game.groovy
@@ -4,18 +4,23 @@ import static java.lang.Math.floor
 
 class Game {
 
-    private int points = 0
+    private Map<String, Integer> points = [:]
+    private List<String> players = []
 
     Game(String player1Name, String player2Name) {
+        points << [("$player1Name".toString()): 0]
+        points << [("$player2Name".toString()): 0]
 
+        players << player1Name
+        players << player2Name
     }
 
     void pointWonBy(String playerName) {
-        points++
+        points[playerName]++
     }
 
     String score() {
-        "${calculateScoreForPoints(points)}-0"
+        "${calculateScoreForPoints(points[players[0]])}-${calculateScoreForPoints(points[players[1]])}"
     }
 
     private int calculateScoreForPoints(int p) {
