e496df4905ae22c2bdb2d2db3fdb009991bd7814
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-02-01 12:31:05 -0200

    Add production code to make test pass

--- tennis/src/main/groovy/au/com/dius/tennis/Game.groovy
+++ tennis/src/main/groovy/au/com/dius/tennis/Game.groovy
@@ -2,15 +2,17 @@ package au.com.dius.tennis
 
 class Game {
 
+    private String score = "0-0"
+
     Game(String player1Name, String player2Name) {
 
     }
 
     void pointWonBy(String playerName) {
-
+        score = "15-0"
     }
 
     String score() {
-        "0-0"
+        score
     }
 }
