b20f59ae025bdd9963ac4ab43fbf481aeb2ebac3
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-02-01 18:03:45 -0200

    Refactored Game to remove the list of players names

--- tennis/src/main/groovy/au/com/dius/tennis/Game.groovy
+++ tennis/src/main/groovy/au/com/dius/tennis/Game.groovy
@@ -5,14 +5,10 @@ import static java.lang.Math.floor
 class Game {
 
     private Map<String, Integer> points = [:]
-    private List<String> players = []
 
     Game(String player1Name, String player2Name) {
         points << [("$player1Name".toString()): 0]
         points << [("$player2Name".toString()): 0]
-
-        players << player1Name
-        players << player2Name
     }
 
     void pointWonBy(String playerName) {
@@ -20,10 +16,10 @@ class Game {
     }
 
     String score() {
-        if (points[players[0]] == 3 && points[players[1]] == 3) {
+        if (points[points.keySet()[0]] == 3 && points[points.keySet()[1]] == 3) {
             return "Deuce"
         }
-        return "${calculateScoreForPoints(points[players[0]])}-${calculateScoreForPoints(points[players[1]])}"
+        return "${calculateScoreForPoints(points[points.keySet()[0]])}-${calculateScoreForPoints(points[points.keySet()[1]])}"
     }
 
     private int calculateScoreForPoints(int p) {
