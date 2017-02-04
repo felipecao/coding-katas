ae5679e56617b674b888b5bf915b4f31f85b458f
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-02-04 17:37:15 -0300

    Strategies refactoring, step 4: refactor AdvantageStrategy to make use of AdvantageRules.

--- tennis/src/main/groovy/au/com/dius/tennis/strategy/AdvantageStrategy.groovy
+++ tennis/src/main/groovy/au/com/dius/tennis/strategy/AdvantageStrategy.groovy
@@ -1,19 +1,18 @@
 package au.com.dius.tennis.strategy
 
 import au.com.dius.tennis.Score
+import au.com.dius.tennis.rules.AdvantageRules
 
 class AdvantageStrategy extends AbstractStrategy {
 
     protected static final String ADVANTAGE = "Advantage"
-    private static final Integer POINTS_DIFFERENCE_FOR_ADVANTAGE = 1
 
     AdvantageStrategy(Score s) {
         this.score = s
     }
 
     boolean isApplicableToScore() {
-        return score.bothPlayersHaveAtLeast3Points() &&
-                score.differenceInPointsIs(POINTS_DIFFERENCE_FOR_ADVANTAGE)
+        AdvantageRules.isAdvantage(score)
     }
 
     String displaySpecificScore() {
