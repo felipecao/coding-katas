e90d81a0347c8e7536a17016bf1caeadbdce9c44
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-02-04 17:29:00 -0300

    Strategies refactoring, step 2: refactor VictoryStrategy to use VictoryRules.

--- tennis/src/main/groovy/au/com/dius/tennis/strategy/VictoryStrategy.groovy
+++ tennis/src/main/groovy/au/com/dius/tennis/strategy/VictoryStrategy.groovy
@@ -1,20 +1,18 @@
 package au.com.dius.tennis.strategy
 
 import au.com.dius.tennis.Score
+import au.com.dius.tennis.VictoryRules
 
 class VictoryStrategy extends AbstractStrategy {
 
     protected static final String VICTORY = "wins"
-    private static final Integer MINIMUM_POINTS_FOR_VICTORY = 4
-    private static final Integer POINTS_DIFFERENCE_FOR_VICTORY = 2
 
     VictoryStrategy(Score s) {
         this.score = s
     }
 
     boolean isApplicableToScore() {
-        return score.anyPlayerHasScoredMoreThanOrEqualsToPoints(MINIMUM_POINTS_FOR_VICTORY) &&
-                score.differenceInPointsIsGreaterThanOrEqualsTo(POINTS_DIFFERENCE_FOR_VICTORY)
+        VictoryRules.isVictory(score)
     }
 
     String displaySpecificScore() {
