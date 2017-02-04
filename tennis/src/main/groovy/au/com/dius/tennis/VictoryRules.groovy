52535973f157bff13b7357229eb3da166b4e79d5
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-02-04 17:28:00 -0300

    Strategies refactoring, step 1: add Rules class to hold the rules that define a victory.

---
+++ tennis/src/main/groovy/au/com/dius/tennis/VictoryRules.groovy
@@ -0,0 +1,12 @@
+package au.com.dius.tennis
+
+class VictoryRules {
+
+    private static final Integer MINIMUM_POINTS_FOR_VICTORY = 4
+    private static final Integer POINTS_DIFFERENCE_FOR_VICTORY = 2
+
+    static boolean isVictory(Score score) {
+        score.anyPlayerHasScoredMoreThanOrEqualsToPoints(MINIMUM_POINTS_FOR_VICTORY) &&
+                score.differenceInPointsIsGreaterThanOrEqualsTo(POINTS_DIFFERENCE_FOR_VICTORY)
+    }
+}
---
+++ tennis/src/test/groovy/au/com/dius/tennis/VictoryRulesTest.groovy
@@ -0,0 +1,32 @@
+package au.com.dius.tennis
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static au.com.dius.tennis.PlayersNames.FIRST_PLAYER
+import static au.com.dius.tennis.PlayersNames.SECOND_PLAYER
+import static au.com.dius.tennis.random.RandomIntegerLessThan50.lessThan
+
+class VictoryRulesTest extends Specification {
+
+    @Unroll
+    def "isVictory returns true when one of the players has scored at least 4 points and has an advantage of two points to the other player"() {
+        given:
+        Score score = new Score(
+                new Player(FIRST_PLAYER, player1Points),
+                new Player(SECOND_PLAYER, player2Points)
+        )
+
+        expect:
+        isVictory == VictoryRules.isVictory(score)
+
+        where:
+        player1Points | player2Points | isVictory
+        lessThan(4)   | lessThan(4)   | false
+        4             | 3             | false
+        5             | 5             | false
+        5             | 3             | true
+        3             | 5             | true
+        7             | 5             | true
+    }
+}
